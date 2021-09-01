510bfc61c162ca2b4b7b53d696ef1774c941eca9
YuliaDenysiuk <yulia.denysiuk@gmail.com>
2021-09-01 23:00:40 +0300

    Настраивает Parcel

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './css/styles.css';
+import { error, defaults } from '../node_modules/@pnotify/core/dist/PNotify.js';
+// import API from './js/apiService';
---
+++ src/js/apiService.js
@@ -0,0 +1,5 @@
+// function fetchImage() {
+//     return fetch('https://pixabay.com/api/?key=23195286-789ed49c86d3fd3c443dc5a81&image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12')
+// }
+
+// export default { fetchImage };
